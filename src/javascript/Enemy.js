6fda96e8961274c00b302cad7de5f3ba555176ca
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-05-20 10:08:00 -0400

    add enemy class

---
+++ src/javascript/Enemy.js
@@ -0,0 +1,11 @@
+class Enemy {
+    constructor(x, y, imageNumber) {
+        this.x = x;
+        this.y = y;
+        this.width = 44;
+        this.height = 32;
+        this.image = new Image();
+        this.image.src = 'src/assests/imagens/enemy${imageNumber}';
+    }
+    
+}
