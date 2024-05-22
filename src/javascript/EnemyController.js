916bc32a27e89ffc873f41bc66e5eb51979e2376
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-05-22 10:52:04 -0400

    add MoveDownTimer function

--- src/javascript/EnemyController.js
+++ src/javascript/EnemyController.js
@@ -1,40 +1,65 @@
-import Enemy from './Enemy.js';
-import MovingDirection from ./MovingDirection.js;
+import Enemy from './Enemy.js'
+import MovingDirection from './MovingDirection.js'
 export default class EnemyController {
     enemyMap = [
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2,],
-        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2,],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2,],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     ];
     enemyRows = [];
 
-    currentDirection = MovingDirection.right;
-    xVelocity = 0;
-    yVelocity = 0;
-    defaultXVelocity = 1;
-    defaultYVelocity = 1;
-    moveDownTimerDefault = 30;
-    moveDownTimer = this.moveDownTimerDefault;
-    fireBulletTimerDefault = 100;
-    fireBulletTimer = this.fireBulletTimerDefault;
+currentDirection  = MovingDirection.right;
+xVelocity = 0;
+yVelocity = 0;
+defaultXVelocity = 1;
+defaultYVelocity = 1;
+moveDownTimerDefault = 30;
+moveDownTimer = this.moveDownTimerDefault;
+fireBulletTimerDefault = 100;
+fireBulletTimer = this.fireBulletTimerDefault;
 
-    constructor(canvas, enemyBulletController, playerBulletController) {
-        this.canvas = canvas;
-        this.enemyBulletController = enemyBulletController;
-        this.playerBulletController = playerBulletController;
-        this.enemyDeathSound = new Audio("src/assests/sounds/enemy-death.wav");
-        this.enemyDeathSound.volume = 0.1;
+constructor(canvas, enemyBulletController, playerBulletController) {
+    this.canvas = canvas;
+    this.enemyBulletController = enemyBulletController;
+    this.playerBulletController = playerBulletController;
+    this.enemyDeathSound = new Audio("src/assets/sounds/enemy-death.wav");
+    this.enemyDeathSound.volume = 0.1;
+    } 
 
-        collisionDetection() {
-            this.enemyRows.forEach((enemyRow) => {
-                enemyRow.forEach(enemy, enemyIndex) => {
-                    if(this.playerBulletController.collideWith(enemy)){
-                    }this.enemy
+    collisionDetection(){
+        this.enemyRows.forEach((enemyRow) => {
+            enemyRow.forEach((enemy, enemyIndex) =>{
+                if (this.playerBulletController.collisionWith(enemy)) {
+                    this.enemyDeathSound.currentTime = 0;
+                    this.enemyDeathSound.play();
+                    enemyRow.splice(enemyIndex, 1);
                 }
-            })
-        }
+            });
+        });
+        this.enemyRows = this.enemyRows.filter((enemyRow) => enemyRow.lenght > 0);
     }
-}
+    fireBullet(){
+        this.fireBulletTimer--;
+        if(this.fireBulletTimerTimer <= 0) {
+            const allEnemies = this.enemyRows.flat()
+            const enemyIndex = Math.floor(Math.random() * allEnemies.lenght);
+            const enemy = allEnemies[enemyIndex];
+            this.enemyBulletController.shoot(enemy.x + enemy.width / 2, enemy.y, -3);
+
+            resetMoveDownTimer() 
+                if(this.moveDownTimer <= 0) {
+                    this.moveDownTimer = this.moveDownTimerDefault;
+                }
+                decrementMoveDownTimer()
+                    if(
+                        this.currentDirection === MovingDirection.downLeft ||
+                        this.currentDirection === MovingDirection.downRight 
+                        ) {
+                        this.moveDownTimer--;
+                    
+                }
+            }
+        }
+     }
